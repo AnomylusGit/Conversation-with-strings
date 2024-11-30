caba2256b1e4a2a169006af35bda80d40e9f4686
AnomylusGit <urath.aarush13@gmail.com>
2024-11-30 14:54:12 +0200

    Conversation with strings

---
+++ main.py
@@ -0,0 +1,16 @@
+name="Aadya"
+age=1
+weight=13.67
+studies=False
+
+print("My sister's name is",name)
+print("Data type of my sister's name is",type(name))
+
+print("My sister's age is",age)
+print("Data type of my sister's age is",type(age))
+
+print("My sister's weight is",weight)
+print("Data type of my sister's weight is",type(weight))
+
+print("Does my sister study?",studies)
+print("Data type of does my sister study is",type(studies))
